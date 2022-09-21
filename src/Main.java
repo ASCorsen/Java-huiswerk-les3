2bf0d6f45102bfb94f34196db06caed22bee1e14
Andrew Corsen <andrewcorsen@andrews-mbp.home>
2022-09-22 00:55:11 +0200

    update tot stap 11

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    Integer [] numeric = {1,2,3,4,5,6,7,8,9,0};
+    String [] alphabetic = {"een","twee","drie","vier","vijf","zes", "zeven", "acht","negen", "null"};
+
+    public static void main(String[] args) {
+        System.out.println("**** Start Terminal ****");
+
+    }
+}
